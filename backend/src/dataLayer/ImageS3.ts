aefb51b8c488201da73dcee827a09b3d71791bed
sidrai97 <siddhantraisgr@gmail.com>
2021-03-18 18:20:28 +0530

    [add] Logging in S3 datalayer

--- backend/src/dataLayer/ImageS3.ts
+++ backend/src/dataLayer/ImageS3.ts
@@ -3,6 +3,10 @@ import 'source-map-support/register'
 import * as AWS from 'aws-sdk'
 import * as AWSXRay from 'aws-xray-sdk'
 
+import { createLogger } from '../utils/logger'
+
+const logger = createLogger('ImageS3')
+
 const XAWS = AWSXRay.captureAWS(AWS)
 
 export class ImageS3 {
@@ -14,11 +18,13 @@ export class ImageS3 {
     ) { }
 
     async getAttachmentUrl(attachmentId: string): Promise<string> {
+        logger.info(`Get S3 Attachement URL for ${attachmentId}`)
         const attachmentUrl = `https://${this.bucketName}.s3.amazonaws.com/${attachmentId}`
         return attachmentUrl
     }
 
     async getUploadUrl(attachmentId: string): Promise<string> {
+        logger.info(`Get S3 Signed URL for ${attachmentId}`)
         const uploadUrl = this.s3.getSignedUrl('putObject', {
             Bucket: this.bucketName,
             Key: attachmentId,
