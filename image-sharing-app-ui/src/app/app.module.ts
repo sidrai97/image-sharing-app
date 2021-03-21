df2d48d10336519b32d5d1d3d3a1d4e56fe56288
sidrai97 <siddhantraisgr@gmail.com>
2021-03-21 13:56:27 +0530

    [add] angular material import and theme selection

--- image-sharing-app-ui/src/app/app.module.ts
+++ image-sharing-app-ui/src/app/app.module.ts
@@ -1,9 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatNativeDateModule } from "@angular/material/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from "@angular/material/form-field";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from '@auth0/auth0-angular';
+import { MaterialModule } from "./material.module";
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { environment } from 'src/environments/environment';
@@ -17,7 +23,12 @@ import { environment } from 'src/environments/environment';
   imports: [
     BrowserModule,
     AppRoutingModule,
-
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    MaterialModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
     AuthModule.forRoot({
       domain: environment.auth0Domain,
       clientId: environment.auth0ClientId,
@@ -25,7 +36,12 @@ import { environment } from 'src/environments/environment';
       scope: 'openid'
     }),
   ],
-  providers: [],
+  providers: [
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: { appearance: "fill" }
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- image-sharing-app-ui/src/environments/environment.ts
+++ image-sharing-app-ui/src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   auth0Domain: 'sidrai97.auth0.com',
   auth0ClientId: '57NEXTdSfHI7dgtk0l2rGA1fHyF6RmDA',
   auth0CallbackURL: 'http://localhost:4200',
-  api: 'https://ygamgxvagh.execute-api.us-east-2.amazonaws.com/dev/tags',
+  api: 'https://ygamgxvagh.execute-api.us-east-2.amazonaws.com/dev/',
   production: false
 };
