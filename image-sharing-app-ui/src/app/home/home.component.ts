709fb61b692b65618a1a0ebe524fd9e38d7ff1d8
sidrai97 <siddhantraisgr@gmail.com>
2021-03-21 20:34:12 +0530

    [add] component to display all the tags and images using mat card and mat chip [add] handling add tag and image mat-dialog responses

--- image-sharing-app-ui/src/app/home/home.component.ts
+++ image-sharing-app-ui/src/app/home/home.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { CreateImageComponent } from '../create-image/create-image.component';
+import { CreateTagComponent } from '../create-tag/create-tag.component';
+import { ApiService } from '../services/api.service';
+import { UserDetailsService } from '../services/user-details.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +12,130 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  isAuthenticated: boolean = false;
+  images: any;
+  tags: any;
+  chipColor = ['primary', 'accent', 'warn']
+  userImagesOnly: boolean = false;
+
+  constructor(public userDetails: UserDetailsService, private api: ApiService, public dialog: MatDialog) { }
 
   ngOnInit(): void {
+    this.userDetails.getIsAuthenticated.subscribe(value => {
+      if (this.isAuthenticated != value) {
+        this.isAuthenticated = value
+        if (this.isAuthenticated) {
+          this.initData();
+        }
+      }
+    })
   }
 
+  initData() {
+    this.api.getTags().subscribe(resp => {
+      this.tags = resp.items
+      this.getImages()
+    })
+  }
+
+  filterImagesByTag(tag: any) {
+    this.api.getImagesByTagId(tag.tagId).subscribe(resp => {
+      this.images = resp.items
+    })
+  }
+
+  filterImagesByUserId() {
+    this.api.getImagesByUserId().subscribe(resp => {
+      this.images = resp.items
+    })
+  }
+
+  getTagNameFromTagId(tagId: string) {
+    let tag = this.tags.filter(tag => tag.tagId == tagId)
+    return tag[0].tagName
+  }
+
+  getAllImages() {
+    this.api.getImages().subscribe(resp => {
+      this.images = resp.items
+    })
+  }
+
+  toggle(event: any) {
+    this.userImagesOnly = event.checked
+    this.getImages()
+  }
+
+  deleteImage(imageId: string) {
+    this.api.deleteImage(imageId).subscribe(resp => {
+      this.getImages()
+    })
+  }
+
+  getImages() {
+    if (this.userImagesOnly) {
+      this.filterImagesByUserId()
+    }
+    else {
+      this.getAllImages()
+    }
+  }
+
+  updateImage(image: any) {
+    const dialogRef = this.dialog.open(CreateImageComponent, {
+      maxHeight: "100%",
+      width: "400px",
+      maxWidth: "100%",
+      data: {
+        type: 'update',
+        tags: JSON.parse(JSON.stringify(this.tags)),
+        image: JSON.parse(JSON.stringify(image))
+      },
+      disableClose: true,
+      hasBackdrop: true
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result.event == 'updated') {
+        this.getImages()
+      }
+    });
+  }
+
+  createImage() {
+    const dialogRef = this.dialog.open(CreateImageComponent, {
+      maxHeight: "100%",
+      width: "400px",
+      maxWidth: "100%",
+      data: {
+        type: 'add',
+        tags: JSON.parse(JSON.stringify(this.tags))
+      },
+      disableClose: true,
+      hasBackdrop: true
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result.event == 'created') {
+        this.getImages()
+      }
+    });
+  }
+
+  createTag() {
+    const dialogRef = this.dialog.open(CreateTagComponent, {
+      maxHeight: "100%",
+      width: "400px",
+      maxWidth: "100%",
+      data: {},
+      disableClose: true,
+      hasBackdrop: true
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result.event == 'created') {
+        this.initData()
+      }
+    });
+  }
 }
